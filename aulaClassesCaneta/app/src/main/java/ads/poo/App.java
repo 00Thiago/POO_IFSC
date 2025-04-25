fb3ac8425b77b8f990f5bbb6fbde52d90fb13db1
Thiago <www.thiago@hotmail.com>
2025-04-25 06:51:22 -0300

    atualização de comentário

--- aulaClassesCaneta/app/src/main/java/ads/poo/App.java
+++ aulaClassesCaneta/app/src/main/java/ads/poo/App.java
@@ -6,11 +6,11 @@ class App {
         Caneta bic = new Caneta(); // Bic é o objeto aqui sendo criado. "New" indica novo objeto
         
         System.out.println(bic.isAberta());
-        bic.abrirFechar();
+        bic.abrirFechar(); // chama o método abrirFechar definido wm Caneta.java para alterar o estado (valor atual do atributo)
         System.out.println(bic.isAberta());
         
 
-        bic.definirCor("branco");
+        bic.definirCor("branco"); // informa o valor/estado para variável privada da classe Caneta chamada "cor", pelo método definirCor
         System.out.println(bic.obterCor());
         // Objeto (bic) chama um método (definirCor) e envia uma mensagem (branco).
             // OU  o código cliente (App) envia uma mensagem (chamada de método definirCor) para o objeto (bic) com um parâmetro ("branco").
