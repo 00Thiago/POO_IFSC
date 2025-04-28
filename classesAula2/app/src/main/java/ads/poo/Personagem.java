921c2076f4915ee9190e6705a5e2fb4527bb3aeb
Thiago Dietrich <thiago.d171@aluno.ifsc.edu.br>
2025-04-28 10:39:32 -0300

    início atividade personagens de jogo

--- classesAula2/app/src/main/java/ads/poo/Personagem.java
+++ classesAula2/app/src/main/java/ads/poo/Personagem.java
@@ -1,8 +1,8 @@
-// Modele um personagem de um jogo de aventura
-// Quais são os atributos e métodos essenciais para esse personagem?
-// Implemente uma classe Java para representar esse personagem
-// Crie uma outra classe com o método main e instancie um objeto da classe Personagem
-
+/*PERSONAGEM 1
+*jogo de aventura
+*Atributos e métodos essenciais para esse personagem
+*Crie classe com o método main e instancie um objeto da classe Personagem
+*/
 
 package ads.poo;
 
@@ -11,13 +11,55 @@ public class Personagem {
     private String arma;
     private int energia;
     private String tipo;
+    private double habilidade;
+    private String pais;
 
+    public void selecao(int escolha) {
+    int escolhido = escolha;
+    }
+
+    public void um() {
+        arma = "espada";
+        energia = 80;
+        tipo = "cavaleiro";
+        habilidade = 20.3;
+        pais = "Escócia";
+    }
+
+    public void dois() {
+        arma = "revolver";
+        energia = 40;
+        tipo = "soldado";
+        habilidade = 47.8;
+        pais = "Alemanha";
+    }
+
+    public void tres() {
+        return (
+        arma = "tacape";
+        energia = 90;
+        tipo = "humano";
+        habilidade = 32.8;
+        pais = "Bósnia";
+        )
+    }
 
     // MÉTODO
 
-    public void atirar(String tipoArma) {
-        arma = tipoArma;
+
+    public String opcao() {
+        if (escolhido == 1) {
+            return um();
+        } else if (escolhido == 2) {
+            return dois();
+        } else if (escolhido == 3) {
+            return tres();
+        } else {
+            return (String erro = "Opção não localizada!");
+        }
     }
 
 
 }
+
+
